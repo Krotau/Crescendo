f55ab295c3400094672312a898859365cae79000
Tim Smeets <tla.smeets@gmail.com>
2025-05-28 14:44:45 +0200

    updated title for webpage client

--- app/rsbuild.config.ts
+++ app/rsbuild.config.ts
@@ -3,6 +3,9 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  html: {
+    title: 'Crescendo',
+  },
   output: {
     cleanDistPath: true,
     assetPrefix: '/static/',
